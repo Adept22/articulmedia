3ab94bce6bd373700b9e8130295d7d195458992a
Adept22 <asdof71@yandex.ru>
2019-01-30 16:25:50 +0300

    Merge

--- bitrix/components/am/search/.desription.php
+++
@@ -1,6 +0,0 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-$arComponentDescription = array(
-     "NAME" => GetMessage("Поиск записей инфоблока"),
-     "DESCRIPTION" => GetMessage("Поиск и вывод записей инфоблока"),
-);
-?>
