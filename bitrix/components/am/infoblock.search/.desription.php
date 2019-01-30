860e866637079ae0ace64fe71bcbebf3044dfa7e
Adept22 <asdof71@yandex.ru>
2019-01-30 16:25:27 +0300

    Rename directory

---
+++ bitrix/components/am/infoblock.search/.desription.php
@@ -0,0 +1,6 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+     "NAME" => GetMessage("Поиск записей инфоблока"),
+     "DESCRIPTION" => GetMessage("Поиск и вывод записей инфоблока"),
+);
+?>
